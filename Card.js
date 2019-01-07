4e0486ae151c11718c5a0f277674f06c1fbfd977
ColinRoberts <colinroberts96@gmail.com>
2019-01-07 18:37:01 -0500

    Interface setup

--- Card.js
+++ Card.js
@@ -1,45 +1,74 @@
 // Public final static values
 // Note: If this were implemented in a better object oriented language, I would make each suit a public static final variable
-var regularSuits = new Set(["spades", "hearts", "diamonds", "clubs"]);
-var nonNumericSuits = new Set(["ace", "jack", "queen", "king"]);
+var validSuits = new Set(["spades", "hearts", "diamonds", "clubs"]);
+var nonNumericCards = new Set(["ace", "jack", "queen", "king"]);
 var validNumbers = new Set([2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-function Card(suit, number) {
+function Card(suit, type) {
 	
 	// Throw errors for invalid cards
-	if (!regularSuits.has(suit) && !nonNumericSuits.has(suit)) {
+	if (!validSuits.has(suit)) {
 		throw "Error: The given suit is not valid.";
 	}
-	if (number != undefined && !validNumbers.has(number)) {
-		throw "Error: The given card number is not valid."
-	}
-	if (nonNumericSuits.has(suit) && number != undefined) {
-		// The numeric value of this card will be overwritten below, so we only need to log a warning
-		console.warn("Warning: Attempting to define a number for a non-numeric suit.");
+	if (!validNumbers.has(type) && !nonNumericCards.has(type)) {
+		throw "Error: The given card-type/number is not valid."
 	}
 	
 	// Private variables
 	// Note: These are not actually protected/private in Javascript, but I would implement them as private in a better OOP language
 	this.suit = suit;
-	this.number = number;
+	this.type = type;
+	this.value;
 	
-	// Define number values for non-numeric suits
-	if () {
+	// Define card value based on the card type
+	if (nonNumericCards.has(type)) {
+		
+		// Assign card value based on the non-numeric type
+		switch(type) {
+			case "ace":
+				this.value = 1;
+				break;
+			case "jack":
+				this.value = 11;
+				break;
+			case "queen":
+				this.value = 12;
+				break;
+			case "king":
+				this.value = 13;
+				break;
+			default:
+				throw "Error: Unknown card type";
+		}
+		
+	} else {
+		
+		// If this card is numeric, its value is just its number
+		this.value = type;
 		
 	}
 	
 }
 
-Card.prototype.getNumber = function() {
-	return this.number;
+Card.prototype.getType = function() {
+	return this.type;
 }
 
 Card.prototype.getSuit = function() {
-	
+	return this.suit;
+}
+
+Card.prototype.getValue = function() {
+	return this.value;
 }
 
 Card.prototype.generateView = function() {
 	// Generate a jQuery object to append to the game interface for this card
 	
 	
+}
+
+// Static function
+Card.prototype.generateReverseView = function() {
+	// Generate a jQuery object to append to the game interface for the backside of a card
 }
--- Game.js
+++ Game.js
@@ -1,12 +1,15 @@
 function Game() {
 	
-	// Sets for the user's hand and winnings pot
-	this.userHand = new Set();
-	this.userPot = new Set();
+	// Declare sets for the user's hand and winnings pot
+	this.userHand;
+	this.userPot;
 	
-	// Sets for the computer's hand and winnings pot
-	this.computerHand = new Set();
-	this.computerPot = new Set();
+	// Declare sets for the computer's hand and winnings pot
+	this.computerHand;
+	this.computerPot;
+	
+	// Start the game
+	this.restart();
 	
 }
 
@@ -20,6 +23,15 @@ Game.prototype.playRound = function() {
 // This function will restart the game with a new shuffled deck
 Game.prototype.restart = function() {
 	
+	// Empty the winning pots
+	this.userPot = new Set();
+	this.computerPot = new Set();
+	
+	// Generate a new shuffled deck and give each player half
+	var deck = this.generateShuffledDeck();
+	this.userHand = new Set(deck.splice(0, 26));
+	this.computerHand = new Set(deck);
+	
 }
 
 // Private
@@ -31,4 +43,16 @@ Game.prototype.finishGame = function() {
 // Static function
 Game.prototype.generateShuffledDeck = function() {
 	
+	var deck = [];
+	
+	validSuits.forEach(function(suit) {
+		validNumbers.forEach(function(type) {
+			deck.push(new Card(suit, type));
+		});
+		nonNumericCards.forEach(function(type) {
+			deck.push(new Card(suit, type));
+		});
+	});
+	
+	return deck;
 }
